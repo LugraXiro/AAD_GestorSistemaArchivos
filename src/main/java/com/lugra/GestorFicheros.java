44b59e10982c4d4a733a34c3aba69cb15acd944f
Lugra <lugra.dev@gmail.com>
2025-10-31 18:35:49 +0100

    Añadidos los 4 primeros requisitos del ejercicio

--- src/main/java/com/lugra/GestorFicheros.java
+++ src/main/java/com/lugra/GestorFicheros.java
@@ -1,6 +1,9 @@
 package com.lugra;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class GestorFicheros {
     private final File carpetaBase;
@@ -21,14 +24,68 @@ public class GestorFicheros {
             return;
         }
 
-        System.out.println("\nContenido de: " + carpetaBase.getAbsolutePath());
-        for (File f : lista) {
+        System.out.println("\nContenido de: " + carpetaBase.getName());
+        listarRecursivo(carpetaBase, 0);
+        
+
+    }
+
+    private void listarRecursivo(File dir, int nivel) {
+        File[] elementos = dir.listFiles();
+        if (elementos == null) return;
+
+        String sangria = "|___".repeat(nivel); // sangría para mostrar jerarquía
+
+        for (File f : elementos) {
             if (f.isDirectory()) {
-                System.out.println("[CARPETA] " + f.getName());
+                System.out.println(sangria + "[DIR]  " + f.getName());
+                listarRecursivo(f, nivel + 1); // llamada recursiva: profundiza un nivel
             } else {
-                System.out.println("[ARCHIVO] " + f.getName());
+                System.out.println(sangria + "[FILE] " + f.getName());
             }
         }
     }
-}
 
+    public void verPermisos(String nombreFichero) {
+        File f = new File(carpetaBase, nombreFichero);
+
+        if (!f.exists()) {
+            System.out.println("El fichero o carpeta no existe dentro de " + carpetaBase.getName());
+            return;
+        }
+
+        System.out.println("\n=== Permisos de " + f.getName() + " ===");
+        System.out.println("Tipo -> " + (f.isDirectory() ? "Directorio" : "Archivo"));
+        System.out.println("Lectura -> " + (f.canRead() ? "Sí" : "No"));
+        System.out.println("Escritura -> " + (f.canWrite() ? "Sí" : "No"));
+        System.out.println("Ejecución: -> " + (f.canExecute() ? "Sí" : "No"));
+    }
+
+    public void leerFichero(String nombreFichero) {
+        try (FileReader fr = new FileReader(nombreFichero)){
+            // try-with-resources -> forma moderna de cerrar el fichero automat. al terminar
+            int c; // guardamos cada carácter como entero entre 0 y 65535
+            while((c = fr.read()) != -1) { // -1 indica fin archivo
+                System.out.print((char) c);
+                // "(char) c" -> convierte el entero "c" en un carácter legible
+            }
+        }catch (IOException e){
+            System.out.println("Error al leer el fichero: " + e.getMessage());
+            System.out.println("Stack trace:");
+            e.printStackTrace();
+        }   
+    }
+
+    public void escribirFichero(String nombreFichero, String contenidoAgregado) {
+        try (FileWriter fw = new FileWriter(nombreFichero, true)) {
+            // true -> modo "append" (añadir al final del fichero)
+            fw.write(contenidoAgregado + "\n");
+            fw.flush(); // asegura que todo se escribe en el fichero
+            System.out.println("Escritura completada en " + nombreFichero);
+        } catch (Exception e) {
+            System.out.println("Error al escribir en el fichero: " + e.getMessage());
+            System.out.println("Stack trace:");
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/lugra/Main.java
+++ src/main/java/com/lugra/Main.java
@@ -16,42 +16,57 @@ public class Main {
 
         do {
            opcion=com.lugra.Menu.pintarMenu();
+           
            switch (opcion) {
-    case 1 -> { System.out.println("\tElegiste opción 1");
-        System.out.println("\tListando archivos y carpetas...");
+                case 1 -> { System.out.println("\tElegiste opción 1:");
+                    System.out.println("\tListando archivos y carpetas...\n");
 
-        gestorF.listarArchivosYCarpetas();
-    }
-    case 2 -> { System.out.println("\tElegiste opción 2");
-        System.out.println("\tVer los permisos del fichero...");
-    }
-    case 3 -> { System.out.println("\tElegiste opción 3");
-        System.out.println("\tLeer el fichero...");
-    }
-    case 4 -> { System.out.println("\tElegiste opción 4");
-        System.out.println("\tEscribir en el fichero...");
-    }
-    case 5 -> { System.out.println("\tElegiste opción 5");
-        System.out.println("\tCrear un fichero...");
-    }
-    case 6 -> { System.out.println("\tElegiste opción 6");
-        System.out.println("\tBorrar un fichero...");
-    }
-    case 7 -> { System.out.println("\tElegiste opción 7");
-        System.out.println("\tCrear un directorio...");
-    }
-    case 8 -> { System.out.println("\tElegiste opción 8");
-        System.out.println("\tBorrar un directorio...");
-    }
-    case 9 -> { System.out.println("\tElegiste opción 9");
-        System.out.println("\tMostrar la ruta absoluta de un fichero...");
-    }
-    case 0 -> { System.out.println("\tElegiste opción 0");
-        salir = true;
-        System.out.println("\tSaliendo del programa...");
-    }
-    default -> System.out.println("Opción incorrecta");
+                    gestorF.listarArchivosYCarpetas();
+                }
+                case 2 -> { System.out.println("\tElegiste opción 2:");
+                    System.out.println("\tVer los permisos del fichero...\n");
+
+                    gestorF.verPermisos(pedirNombre());
+                }
+                case 3 -> { System.out.println("\tElegiste opción 3:");
+                    System.out.println("\tLeer el fichero...\n");
+
+                    gestorF.leerFichero("src\\main\\java\\com\\lugra\\dir_playground\\" + pedirNombre());
+                }
+                case 4 -> { System.out.println("\tElegiste opción 4:");
+                    System.out.println("\tEscribir en el fichero...\n");
+
+                    String contenidoAgregado = "Línea agregada desde el gestor de ficheros.";
+                    gestorF.escribirFichero("src\\main\\java\\com\\lugra\\dir_playground\\" + pedirNombre(), contenidoAgregado);
+                }
+                case 5 -> { System.out.println("\tElegiste opción 5:");
+                    System.out.println("\tCrear un fichero...\n");
+
+                    
+                }
+                case 6 -> { System.out.println("\tElegiste opción 6:");
+                    System.out.println("\tBorrar un fichero...\n");
+                }
+                case 7 -> { System.out.println("\tElegiste opción 7:");
+                    System.out.println("\tCrear un directorio...\n");
+                }
+                case 8 -> { System.out.println("\tElegiste opción 8:");
+                    System.out.println("\tBorrar un directorio...\n");
+                }
+                case 9 -> { System.out.println("\tElegiste opción 9:");
+                    System.out.println("\tMostrar la ruta absoluta de un fichero...\n");
+                }
+                case 0 -> { System.out.println("\tElegiste opción 0:");
+                    salir = true;
+                    System.out.println("\tSaliendo del programa...\n");
+                }
+                default -> System.out.println("Opción incorrecta");
 }
         } while (!salir);
     }  // fin main  
+
+    static public String pedirNombre() {
+        System.out.print("Introduce el nombre: ");
+        return teclado.nextLine().trim();
+    }
 }
